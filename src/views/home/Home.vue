2e254cf56a749dfc5fb2af3bdfeb96021ebb8acb
kongjun0320 <1741142595@qq.com>
2019-07-02 13:59:11 +0800

    首页header初始化完成

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,9 +1,15 @@
 <template>
-    <div class="home">home</div>
+    <div class="home">
+        <Hh></Hh>
+    </div>
 </template>
 <script>
+import Hh from './components/Hh'
 export default {
-    name:'Home'
+    name:'Home',
+    components:{
+        Hh
+    }
 }
 </script>
 <style scoped>
---
+++ src/views/home/components/Hh.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="hh">
+        <section class="left">
+            <div class="icon_back">
+                <i class="iconfont">&#xe624;</i>
+            </div> 
+        </section>
+        <section class="input">
+            <i class="iconfont">&#xe632;</i>
+            输入城市/景点/主题
+        </section>
+        <section class="right">
+            城市
+            <i class="iconfont arrow-down">&#xe6aa;</i>
+        </section>
+    </div>
+</template>
+<script>
+export default {
+    name:'Hh'
+}
+</script>
+<style lang="stylus" scoped>
+    @import '~@/assets/styles/varibles.styl';
+    .hh 
+        height .86rem;
+        line-height .86rem;
+        background $baColor;
+        color #fff;
+        display flex;
+        .left 
+            width .64rem;
+            float left;
+            .icon_back
+                text-align center;
+                font-size .2rem
+        .input 
+            flex 1;
+            border-radius .1rem;
+            margin-top .12rem;
+            margin-left .2rem
+            padding-left .2rem
+            background #fff;
+            height .64rem;
+            line-height .64rem;
+            color #ccc;
+        .right 
+            width 1.24rem;
+            float right;
+            text-align center;
+            .arrow-down
+                margin-left -.04rem;
+                font-size .30rem;
+</style>
+
+
